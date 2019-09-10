1a186609bad70093f90219db1a2ae682e1c0aa98
Francisco Serra <francisco.d.serra@gmail.com>
2019-09-10 22:51:55 +0100

    newSale

--- src/main/java/Interface/IProductServices.java
+++ src/main/java/Interface/IProductServices.java
@@ -4,14 +4,30 @@ import model.Product;
 
 import java.util.List;
 
+/**
+ * Interface de servicços relacionados com produtos
+ */
 public interface IProductServices {
 
+    /**
+     * @return Serviço que retorna da base de dados a listagem de produtos
+     */
     List<Product> getProductsService();
 
+    /**
+     * @param prodId Id de Produto
+     * @return Serviço retorna um produto com base no Id inserido
+     */
     Product getProductService(int prodId);
 
+    /**
+     * @param product Produto a ser inserido na base de dados
+     */
     void createProductService(Product product);
 
+    /**
+     * @param product Produto a ser atualizado na base de dados
+     */
     void updateProductService(Product product);
 
 }
--- src/main/java/Interface/ISaleServices.java
+++ src/main/java/Interface/ISaleServices.java
@@ -5,15 +5,39 @@ import model.Sale;
 
 import java.util.List;
 
+/**
+ * Interface de Serviços de produtos que liga os controladores das views à base de dados
+ */
 public interface ISaleServices {
 
+    /**
+     * @return Retorna a Listagem de Vendas
+     */
     List<Sale> getSalesService();
+
+    /**
+     * @return Retorna a LIstagem de encomendas
+     */
     List<Order> getOrdersService();
 
+    /**
+     * @param saleId Id de Vendas
+     * @return Com um Id de Vendas vai a base de dados buscar uma venda
+     */
     Sale getSaleService(int saleId);
 
+    /**
+     * @param sale Venda a ser inserida na base de Dados
+     */
     void createSaleService(Sale sale);
+
+    /**
+     * @param sale Encomenda a ser inserida na base de dados
+     */
     void createSaleService(Order sale);
 
+    /**
+     * @param sale Atualizar uma venda na base de dados
+     */
     void updateSaleService(Sale sale);
 }
